409db2f6d9d8403d03c0a686f084fb49e963bb5d
schang94 <mc_94@naver.com>
2019-08-08 18:05:41 +0900

    2019-08-08

---
+++ src/homework/no2/MemberFindDto.java
@@ -0,0 +1,26 @@
+package homework.no2;
+
+public class MemberFindDto {
+	private int seq;
+	private String email;
+	private String code;
+	private String sdate;
+	private String edate;
+	
+	// 생성자 만들기 (자동 생성 쓰지말고)
+	
+	// seq에 대한 setter 만들기
+	// seq에 대한 getter 만들기
+	
+	// email에 대한 setter 만들기
+	// email에 대한 getter 만들기
+	
+	// code에 대한 setter 만들기
+	// code에 대한 getter 만들기
+	
+	// sdate에 대한 setter 만들기
+	// sdate에 대한 getter 만들기
+	
+	// edate에 대한 setter 만들기
+	// edate에 대한 getter 만들기
+}
---
+++ src/homework/no2/MemberFindDtoDemo.java
@@ -0,0 +1,14 @@
+package homework.no2;
+
+public class MemberFindDtoDemo {
+	// 1. 객체 생성해 보기
+	MemberFindDto m1 = new MemberFindDto();
+	
+	// 2. 객체의 내용을 출력해 본다.
+	
+	// 3. 객체의 내용을 수정해 본다.
+	
+	// 4. 객체의 내용을 출력해 본다.
+	
+	// 5. 1-4번 한번 더 해보기
+}
